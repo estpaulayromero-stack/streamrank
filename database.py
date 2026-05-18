7cd22fdceb5e908b905f966fd999440c9bd44f12
estpaulayromero-stack <est.paulay.romero@unimilitar.edu.co>
2026-05-18 10:05:56 -0500

    correcciones python

--- database.py
+++ database.py
@@ -1,30 +1,22 @@
 import sqlite3
 
-conexion = sqlite3.connect("streamrank.db")
+conn = sqlite3.connect('streamrank.db')
 
-cursor = conexion.cursor()
+cursor = conn.cursor()
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS usuarios (
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS users (
+
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email TEXT UNIQUE,
-    password TEXT,
-    username TEXT
-)
-""")
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS listas (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    usuario_id INTEGER,
-    titulo TEXT,
-    plataforma TEXT,
-    genero TEXT,
-    rating TEXT,
-    descripcion TEXT
-)
-""")
+    email TEXT UNIQUE NOT NULL,
 
-conexion.commit()
+    password TEXT NOT NULL
+)
+''')
+
+conn.commit()
+
+conn.close()
 
 print("Base de datos creada correctamente")
