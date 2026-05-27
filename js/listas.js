00bc8165b8495a223bde233c855defd75f101bdb
estpaulayromero-stack <est.paulay.romero@unimilitar.edu.co>
2026-05-27 07:44:31 -0500

    correccion back

--- js/listas.js
+++ js/listas.js
@@ -9,9 +9,9 @@
 // CONFIG API
 // ==========================
 
-const LISTAS_API = "http://localhost/streamrank/api/listas.php";
-const TOPS_API   = "http://localhost/streamrank/api/Tops.php";
-const HISTORIAL_API = "http://localhost/streamrank/api/historial.php";
+const LISTAS_API = "http://localhost/back-streamRank/api/listas.php";
+const TOPS_API   = "http://localhost/back-streamRank/api/Tops.php";
+const HISTORIAL_API = "http://localhost/back-streamRank/api/historial.php";
 
 // ==========================
 // AGREGAR ÍTEM A LA LISTA
